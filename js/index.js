238c0b53c200230210e2c47e7ede75953e78da50
gilangwokeh <gilangwokeh12@gmail.com>
2022-06-18 08:22:13 +0700

    blooger web

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.menu input')
+const nav = document.querySelector('nav ul')
+
+toggle.addEventListener('click', function () {
+    nav.classList.toggle('slide')
+})
+
+
